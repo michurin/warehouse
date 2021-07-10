90c35553cf49586d893d1a3e0ab085987bcbb50d
Alexey Michurin <a.michurin@gmail.com>
2021-07-10 06:30:16 +0300

    CRX 1.19: Y mail

--- crx/crx-catout/m.js
+++ crx/crx-catout/m.js
@@ -1,9 +1,12 @@
 (() => {
   setInterval(() => {
     // letter-like
-    var cc = document.querySelector('[data-key="box=toolbar-box"]').parentElement.children;
-    if (cc.length === 5) {
-      cc[3].remove();
+    var cc = document.querySelector('[data-key="box=toolbar-box"]');
+    if (cc) {
+      cc = cc.parentElement.children;
+      if (cc.length === 5) {
+        cc[3].remove();
+      }
     }
     // promo
     document.querySelectorAll('[data-key="view=mail-pro-left-column-button"]').forEach(e => e.remove());
@@ -18,5 +21,16 @@
         x.remove();
       }
     }
+    // top ad line
+    cc = document.querySelectorAll('.mail-Layout-Main');
+    if (cc.length === 1) {
+      cc = cc[0].children;
+      if (cc.length === 3) {
+        cc[1].remove();
+      }
+    }
+    // remove annoying and useless garbage
+    document.querySelectorAll('.PSHeaderLogo360').forEach(e => e.remove());
+    document.querySelectorAll('.PSHeader-Center').forEach(e => e.remove());
   }, 2000);
 })();
