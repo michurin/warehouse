b8987e1a12638b8fd7275ab6f45292730e33a38b
Alexey Michurin <a.michurin@gmail.com>
2021-09-25 07:29:12 +0300

    Simple js server (for quick mocks): get data from external files

--- tools/js-server/s.js
+++ tools/js-server/s.js
@@ -1,30 +1,51 @@
-const http = require('http')
-const port = 3000
+const http = require('http');
+const util = require('util');
+const fs = require('fs');
+
+const port = 3000;
+
+let config = [{
+  urlRe: /\/search/,
+  respFile: 'serp.json',
+}];
+
+const logJSON = (v) => {
+  console.log(util.inspect(JSON.parse(v), {
+    depth: null,
+    colors: true,
+    breakLength: 160,
+    sorted: true,
+  }));
+};
 
 const requestHandler = (request, response) => {
-    var data = ''
-    request.on('data', chunk => {
-        data += chunk
-    });
-    request.on('end', () => {
-        d = JSON.stringify({
-            result: {
-              availableVerifications: [{
-                verificationType: "passport",
-                verificationStatusType: "verified",
-              }],
-            },
-        })
-        console.log(request.method, request.url, data, d)
-        response.end(d)
-    })
+  const { headers, method, url } = request;
+  var data = '';
+  request.on('data', chunk => {data += chunk});
+  request.on('end', () => {
+    var resp = undefined;
+    for(var i = 0; i < config.length; i++) {
+      var c = config[i];
+      if (c.urlRe.test(url)) {
+        console.log('matched', c);
+        resp = fs.readFileSync(c.respFile);
+        break;
+      } else {
+        console.log('skipped', c);
+      }
+    }
+    console.log(`\x1b[1;44;36m${url}\x1b[K\x1b[0m`);
+    logJSON(data);
+    logJSON(resp);
+    response.end(resp);
+  });
 }
 
-const server = http.createServer(requestHandler)
+const server = http.createServer(requestHandler);
 
 server.listen(port, (err) => {
     if (err) {
-        return console.log('something bad happened', err)
+        return console.log('something bad happened', err);
     }
-    console.log(`server is listening on ${port}`)
-})
+    console.log(`server is listening on ${port}`);
+});
