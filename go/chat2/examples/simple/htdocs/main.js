826e7babead8c4ba0f104131587630d20ff4531e
Alexey Michurin <a.michurin@gmail.com>
2022-06-26 10:14:02 +0300

    Simple chat: JS

--- go/chat2/examples/simple/htdocs/main.js
+++ go/chat2/examples/simple/htdocs/main.js
@@ -1,30 +1,34 @@
 $(() => {
-  const kit = chatAdapter('/pub', '/sub');
-  const text = $('#text');
-  const name = $('#name');
+  const kit = chatAdapter('/pub', '/sub')
+  const text = $('#text')
+  const name = $('#name')
   text.keypress((e) => {
-    if (e.which == 13) {
+    if (e.which === 13) {
       kit.send({
         text: text.val(),
         name: name.val(),
-      });
-      text.val('');
-      text.focus();
-      return false;
+      })
+      text.val('')
+      text.focus()
+      return false
     }
-  });
+    return true
+  })
   name.keypress((e) => {
-    if (e.which == 13) {
-      text.focus();
-      return false;
+    if (e.which === 13) {
+      text.focus()
+      return false
     }
-  });
-  name.focus();
-  kit.loop((message) => {
-    $('#board').append($('<div>').append(
-      $('<b>').text(message.name + ':'),
-      $('<span>').text(' ' + message.text)
-    ));
-    $("html, body").scrollTop($(document).height() - $(window).height());
-  });
-});
+    return true
+  })
+  name.focus()
+  kit.loop((messages) => {
+    messages.reverse().forEach((msg) => {
+      $('#board').append($('<div>').append(
+        $('<b>').text(`${msg.name}:`),
+        $('<span>').text(` ${msg.text}`)
+      ))
+    })
+    $('html, body').scrollTop($(document).height() - $(window).height())
+  })
+})
--- go/chat2/js/kit.js
+++ go/chat2/js/kit.js
@@ -1,33 +1,29 @@
 const chatAdapter = (pubUrl, subUrl) => {
-  const call = (url, data) => {
-    return fetch(url, {
-      method: 'POST',
-      cache: 'no-cache',
-      body: JSON.stringify(data),
-    });
-  };
-  const sleep = delay => new Promise(r => setTimeout(r, delay));
+  const call = (url, data) => fetch(url, {
+    method: 'POST',
+    cache: 'no-cache',
+    body: JSON.stringify(data),
+  })
+  const sleep = (delay) => new Promise((resolve) => { setTimeout(resolve, delay) })
   return {
     send(message) {
-      call(pubUrl, message);
+      call(pubUrl, message)
     },
     async loop(f) {
-      var bound = -1;
-      for (; ;) {
+      let bound = -1
+      while (true) {
         try {
-          const response = await call(subUrl, { bound });
-          if (response.status != 200) {
-            throw response.status;
+          const response = await call(subUrl, { bound })
+          if (response.status !== 200) {
+            throw response.status
           }
-          const data = await response.json();
-          bound = data.bound;
-          for (i = data.messages.length - 1; i >= 0; i--) {
-            f(data.messages[i]);
-          }
-        } catch {
-          await sleep(2000);
+          const data = await response.json()
+          bound = data.bound
+          f(data.messages)
+        } catch (e) {
+          await sleep(2000)
         }
       }
     },
-  };
-};
+  }
+}
