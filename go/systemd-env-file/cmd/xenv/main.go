080b3e962dda687c47eaabfce6b336447a7abe9a
Alexey Michurin <a.michurin@gmail.com>
2023-06-18 06:29:01 +0300

    Systemd-env-file module: cli tool: improve error messages

--- go/systemd-env-file/cmd/xenv/main.go
+++ go/systemd-env-file/cmd/xenv/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"log"
 	"os"
 	"os/exec"
 
@@ -13,17 +14,17 @@ func app(args []string, stdout, stderr io.Writer) error {
 	if len(args) < 1 {
 		return fmt.Errorf("you are to specify command")
 	}
-	err := (error)(nil)
-	cmd := exec.Command(args[0], args[1:]...)
+	env, err := sdenv.Environ(os.Environ(), "xenv.env")
+	if err != nil {
+		return fmt.Errorf("cannot open env file: %w", err)
+	}
+	cmd := exec.Command(args[0], args[1:]...) //nolint:gosec
 	cmd.Stdout = stdout
 	cmd.Stderr = stderr
-	cmd.Env, err = sdenv.Environ(os.Environ(), "xenv.env")
-	if err != nil {
-		return err
-	}
+	cmd.Env = env
 	err = cmd.Run()
 	if err != nil {
-		return err
+		return fmt.Errorf("cannot run command: %w", err)
 	}
 	return nil
 }
@@ -31,6 +32,6 @@ func app(args []string, stdout, stderr io.Writer) error {
 func main() {
 	err := app(os.Args[1:], os.Stdout, os.Stderr)
 	if err != nil {
-		fmt.Println(err)
+		log.Println("Error:", err)
 	}
 }
