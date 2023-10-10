ad578ed7c5b2f872115d6df520697e1263cdf664
Alexey Michurin <a.michurin@gmail.com>
2023-10-10 07:58:30 +0300

    PPLog: tests: cosmetics and one more example

--- go/pplog/slogtotext/writer_test.go
+++ go/pplog/slogtotext/writer_test.go
@@ -2,6 +2,7 @@ package slogtotext_test
 
 import (
 	"bytes"
+	"fmt"
 	"os"
 	"strings"
 	"testing"
@@ -15,7 +16,7 @@ import (
 //nolint:errcheck
 func ExamplePPLog_justShowingIdea() {
 	w := slogtotext.PPLog(os.Stdout, "", `{{.time}} [{{.pid}}] {{.message}}`, nil, nil, 0)
-	w.Write([]byte(`{"time":"12:00", "pid":11, "message":"OK"}` + "\n"))
+	fmt.Fprintln(w, `{"time":"12:00", "pid":11, "message":"OK"}`)
 	// output:
 	// 12:00 [11] OK
 }
@@ -23,9 +24,9 @@ func ExamplePPLog_justShowingIdea() {
 //nolint:errcheck
 func ExamplePPLog_includeUnknownFields() {
 	w := slogtotext.PPLog(os.Stdout, "", `{{.time}} [{{.pid}}] {{.message}}{{range .UNKNOWN}} {{.K}}={{.V}}{{end}}`, nil, nil, 0)
-	w.Write([]byte(`{"time":"12:00", "pid":11, "message":"OK", "request_id": "xx", "g": {"a": "A", "b": "B"}}` + "\n"))
+	fmt.Fprintln(w, `{"time":"12:00", "pid":11, "message":"OK", "request_id": "xx", "group": {"a": "A", "b": "B"}}`)
 	// output:
-	// 12:00 [11] OK g.a=A g.b=B request_id=xx
+	// 12:00 [11] OK group.a=A group.b=B request_id=xx
 }
 
 //nolint:errcheck
@@ -35,40 +36,60 @@ func ExamplePPLog_includeUnknownFieldsButSkipSomeOfThem() {
 		"pid":     struct{}{},
 		"message": struct{}{},
 		"host":    struct{}{}, // skip field that is not showing up in template
-		"g": map[string]any{
+		"group": map[string]any{
 			"a": struct{}{}, // skip g.a
 		},
 	}, nil, 0)
-	w.Write([]byte(`{"time":"12:00", "pid":11, "message":"OK", "request_id": "xx", "host": "sun", "g": {"a": "A", "b": "B"}}` + "\n"))
+	fmt.Fprintln(w, `{"time":"12:00", "pid":11, "message":"OK", "request_id": "xx", "host": "sun", "group": {"a": "A", "b": "B"}}`)
 	// output:
-	// 12:00 [11] OK g.b=B request_id=xx
+	// 12:00 [11] OK group.b=B request_id=xx
 }
 
 //nolint:errcheck
 func ExamplePPLog_invalidJSON() {
 	w := slogtotext.PPLog(os.Stdout, `INVALID JSON: {{. | printf "%q"}}`, "", nil, nil, 0)
-	w.Write([]byte(`{[} invalid json` + "\n"))
+	fmt.Fprintln(w, `{[} invalid json`)
 	// output:
 	// INVALID JSON: "{[} invalid json"
 }
 
 //nolint:errcheck
-func ExamplePPLog_formatTimestapm() {
+func ExamplePPLog_formatTimestamp() {
 	w := slogtotext.PPLog(os.Stdout, "", `{{.time | tmf "2006-01-02T15:04:05Z" "15:04:05"}} {{.message}}`, nil, nil, 0)
-	w.Write([]byte(`{"time":"2009-11-10T23:00:00Z", "message":"OK"}` + "\n"))
+	fmt.Fprintln(w, `{"time":"2009-11-10T23:00:00Z", "message":"OK"}`)
 	// output:
 	// 23:00:00 OK
 }
 
 //nolint:errcheck
-func ExamplePPLog_noValues() {
-	w := slogtotext.PPLog(os.Stdout, "", `[{{.message}}]`, nil, nil, 0)
-	w.Write([]byte("{}\n"))
-	w = slogtotext.PPLog(os.Stdout, "", `{{if .message}}[{{.message}}]{{else}}[-]{{end}}`, nil, nil, 0)
-	w.Write([]byte("{}\n"))
+func ExamplePPLog_customTemplateFunction() {
+	funcs := map[string]any{
+		"smartCrop": func(a, b any) string {
+			n := a.(int) // don't forget to check
+			s := b.(string)
+			if len(s) > n {
+				return s[:n-3] + "..." // oversimplification: just don't use n < 3
+			}
+			return s
+		},
+	}
+	w := slogtotext.PPLog(os.Stdout, "", `{{.message | smartCrop 10 }}`, nil, funcs, 0)
+	fmt.Fprintln(w, `{"message": "text"}`)
+	fmt.Fprintln(w, `{"message": "very long text"}`)
 	// output:
-	// [<no value>]
-	// [-]
+	// text
+	// very lo...
+}
+
+//nolint:errcheck
+func ExamplePPLog_noValues() {
+	w := slogtotext.PPLog(os.Stdout, "", `[{{.data}}] {{.message}}`, nil, nil, 0)
+	fmt.Fprintln(w, `{"message": "problem"}`)
+	w = slogtotext.PPLog(os.Stdout, "", `[{{if .data}}{{.data}}{{else}}-{{end}}] {{.message}}`, nil, nil, 0)
+	fmt.Fprintln(w, `{"message": "solution"}`)
+	// output:
+	// [<no value>] problem
+	// [-] solution
 }
 
 func nthPermutation[T any](n int, a []T) []T {
