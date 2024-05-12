8d3ab1b1f34130f5077d717aa181515333c7584b
Alexey Michurin <a.michurin@gmail.com>
2024-05-12 17:01:26 +0300

    Telegram bot: flags: -v

--- go/tbot/cmd/cnbot/main.go
+++ go/tbot/cmd/cnbot/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"context"
+	"flag"
 	"os"
 	"os/signal"
 
@@ -12,16 +13,23 @@ import (
 var Build = "development"
 
 func main() {
+	vFlag := flag.Bool("v", false, "show version")
+	flag.Parse()
+	if vFlag != nil && *vFlag {
+		app.ShowVersionInfo()
+		return
+	}
+
 	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
 	defer stop()
 
 	app.SetupLogging()
-	cfg, err := app.LoadConfigs(os.Args[1:]...)
+	cfg, err := app.LoadConfigs(flag.Args()...)
 	if err != nil {
 		xlog.L(ctx, err)
 		return
 	}
-	err = app.Application(ctx, cfg, Build)
+	err = app.Application(ctx, cfg, Build+" "+app.MainVersion())
 	if err != nil {
 		xlog.L(ctx, err)
 		return
