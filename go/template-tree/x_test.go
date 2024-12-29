afdd3332d418201b4f5f18785086460b78c4316c
Alexey Michurin <a.michurin@gmail.com>
2024-12-29 21:28:54 +0300

    Go: text/template: validating plain templates

--- go/template-tree/x_test.go
+++ go/template-tree/x_test.go
@@ -99,7 +99,7 @@ func R(pfx string, nd parse.Node) {
 	}
 }
 
-func Example() {
+func Example_full() {
 	tt := template.Must(template.New("x").Parse(
 		`a {{ true | js }} {{ nil }} {{ .X.Y 99 | js -}}
 		{{ range $v := .A }}{{ $v | js }}{{ continue }}{{ break }}{{ end -}}
@@ -218,4 +218,82 @@ func Example() {
 	//   pipe: cmds
 	//     cmd: "x" 386
 	//       quoted text: "x" ("x")
+}
+
+// V allows only the simplest placeholders (and plain text)
+func V(nd parse.Node) ([]string, error) {
+	lst, ok := nd.(*parse.ListNode)
+	if !ok {
+		return nil, fmt.Errorf("invalid root type %T", nd)
+	}
+	res := []string(nil)
+	for _, nx := range lst.Nodes {
+		switch n := nx.(type) {
+		case *parse.TextNode: // skip text
+		case *parse.ActionNode:
+			p := n.Pipe
+			if p == nil {
+				return nil, fmt.Errorf("pipe is nil: %s", n.String())
+			}
+			if len(p.Decl) > 0 {
+				return nil, fmt.Errorf("declarations are not allowed: %s", n.String())
+			}
+			if len(p.Cmds) != 1 {
+				return nil, fmt.Errorf("only one command is allowed: %s", n.String())
+			}
+			cmd := p.Cmds[0]
+			if len(cmd.Args) != 1 {
+				return nil, fmt.Errorf("expected one argument: %s", n.String())
+			}
+			arg, ok := cmd.Args[0].(*parse.FieldNode)
+			if !ok {
+				return nil, fmt.Errorf("expected field node: %s", n.String())
+			}
+			if len(arg.Ident) != 1 {
+				return nil, fmt.Errorf("expected one-level identifier: %s", n.String())
+			}
+			res = append(res, arg.Ident[0])
+		default:
+			return nil, fmt.Errorf("node type %T is not allowed: %s", n, n.String())
+		}
+	}
+	return res, nil
+}
+
+func Example_superSimpleValidato() {
+	for _, t := range []string{
+		`a {{ .x }}, {{ .y }}`,
+		`a {{ .x.y }}`,
+		`a {{ .x | printf "%q" }}`,
+		`a {{ $x := "" }}`,
+		`a {{ if true }}{{ end }}`,
+	} {
+		fmt.Println("\nTemplate:", t)
+		tt := template.Must(template.New("x").Parse(t))
+		x, err := V(tt.Root)
+		if err != nil {
+			fmt.Println("Error:", err)
+			if x != nil {
+				panic(t)
+			}
+		} else {
+			fmt.Println("Variables:", x)
+		}
+	}
+	// Output:
+	//
+	// Template: a {{ .x }}, {{ .y }}
+	// Variables: [x y]
+	//
+	// Template: a {{ .x.y }}
+	// Error: expected one-level identifier: {{.x.y}}
+	//
+	// Template: a {{ .x | printf "%q" }}
+	// Error: only one command is allowed: {{.x | printf "%q"}}
+	//
+	// Template: a {{ $x := "" }}
+	// Error: declarations are not allowed: {{$x := ""}}
+	//
+	// Template: a {{ if true }}{{ end }}
+	// Error: node type *parse.IfNode is not allowed: {{if true}}{{end}}
 }
