69e0df2de2fade510d1d9d5e87c8dcee5a6d49d2
Alexey Michurin <a.michurin@gmail.com>
2025-02-16 14:41:31 +0300

    Particle Life Simulation: naive 3x3 partitioning

--- docs/particle-life-simulation/main.js
+++ docs/particle-life-simulation/main.js
@@ -12,6 +12,161 @@
 // How to check
 // eslint --no-eslintrc --fix main.js
 
+const partitionSize = 20
+
+const arenaWidth = 40
+const arenaHeight = 40
+
+function createEmptyArena() {
+  const a = []
+  for (let i = 0; i < arenaHeight; i++) {
+    const r = []
+    for (let j = 0; j < arenaWidth; j++) {
+      r.push([])
+    }
+    a.push(r)
+  }
+  return a
+}
+
+let arena = createEmptyArena()
+
+// ---- fill arena
+
+for (let tp = 0; tp < 4; tp++) {
+  for (let s = 0; s < 500; s++) {
+    const x = arenaWidth * Math.random()
+    const y = arenaHeight * Math.random()
+    console.log(x, y)
+    const v = 1 // partitions/second
+    const d = Math.random() * Math.PI * 2
+    arena[Math.floor(y)][Math.floor(x)].push({
+      x: x % 1, // coordinates, related partition
+      y: y % 1,
+      vx: v * Math.cos(d),
+      vy: v * Math.sin(d),
+      tp: tp,
+      clr: `hsl(${Math.round(tp * 360 / 4)} 100% 50%)`
+    })
+  }
+}
+console.log(arena)
+
+// ---- setup canvas
+
+const canvas = document.getElementById('a')
+const context = canvas.getContext('2d')
+
+canvas.width = Math.round(partitionSize * arenaWidth)
+canvas.height = Math.round(partitionSize * arenaHeight)
+
+function drawArena() {
+  context.clearRect(0, 0, partitionSize * arenaWidth, partitionSize * arenaHeight)
+  for (let pr = 0; pr < arenaHeight; pr++) { // loop over partitions rows
+    for (let p = 0; p < arenaWidth; p++) { // loop over partitions
+      const pv = arena[pr][p]
+      for (let i = 0; i < pv.length; i++) { // loop over members of partition
+        const v = pv[i]
+        context.fillStyle = v.clr
+        context.fillRect((p + v.x) * partitionSize - 1, (pr + v.y) * partitionSize - 1, 3, 3)
+      }
+    }
+  }
+}
+
+const r1 = .2
+const r2 = .7
+const forceMatrix = [
+  [.4, .2, 0, 0],
+  [-.05, .4, 0, 0],
+  [0, 0, .4, .2],
+  [0, 0, -.005, .4],
+]
+
+function accelerate(dt) {
+  for (let tpr = 0; tpr < arenaHeight; tpr++) { // loop over target partition rows
+    for (let tp = 0; tp < arenaWidth; tp++) { // loop over target partitions
+      const tpv = arena[tpr][tp]
+      for (let t = 0; t < tpv.length; t++) { // loop over members of target partition
+        const tv = tpv[t]
+        const tx = tv.x + tp
+        const ty = tv.y + tpr
+        let fx = 0
+        let fy = 0
+        for (let spr = tpr - 1; spr < tpr + 2; spr++) { // loop over source partitions rows
+          for (let sp = tp - 1; sp < tp + 2; sp++) { // loop over source partitions
+            const spv = arena[(spr + arenaHeight) % arenaHeight][(sp + arenaWidth) % arenaWidth] // TRICK!
+            for (let s = 0; s < spv.length; s++) { // loop over members of source partition
+              if (tpr === spr && tp === sp && t === s) {
+                continue // do not interact with itself
+              }
+              const sv = spv[s]
+              const sx = sv.x + sp // TRICK!
+              const sy = sv.y + spr
+              const dx = sx - tx
+              const dy = sy - ty
+              const r = Math.hypot(dx, dy)
+              var f = 0
+              if (r < r1) {
+                f = (-1 + r / r1) * 100
+              } else if (r < r2) {
+                f = forceMatrix[tv.tp][sv.tp] * (r - r1) / (r2 - r1) // TODO: pre calculate denominator
+              } else if (r < 1) {
+                f = forceMatrix[tv.tp][sv.tp] * (1 - r) / (1 - r2)
+              }
+              fx += f * dx / r
+              fy += f * dy / r
+            }
+          }
+        }
+        fx -= tv.vx * .8 // TODO
+        fy -= tv.vy * .8
+        tv.vx += dt * fx
+        tv.vy += dt * fy
+      }
+    }
+  }
+}
+
+function moveArena(dt) {
+  const a = createEmptyArena()
+  for (let pr = 0; pr < arenaHeight; pr++) { // loop over partitions rows
+    for (let p = 0; p < arenaWidth; p++) { // loop over partitions
+      const pv = arena[pr][p]
+      for (let i = 0; i < pv.length; i++) { // loop over members of partition
+        const v = pv[i]
+        const x = (p + v.x + dt * v.vx + arenaWidth) % arenaWidth
+        const y = (pr + v.y + dt * v.vy + arenaHeight) % arenaHeight
+        v.x = x % 1
+        v.y = y % 1
+        a[Math.floor(y)][Math.floor(x)].push(v)
+      }
+    }
+  }
+  arena = a
+}
+
+let prevMs = 0
+function animate(ms) {
+  if (prevMs === 0) {
+    prevMs = ms
+  }
+  let dt = (ms - prevMs) / 1000 * 20 // TODO time factor
+  if (dt !== 0) { // TODO if dt is too big, skip too?
+    if (dt > .015) {
+      dt = .015
+    }
+    accelerate(dt)
+    moveArena(dt)
+    drawArena()
+    prevMs = ms
+  }
+  window.requestAnimationFrame(animate)
+}
+
+window.requestAnimationFrame(animate)
+
+/*
 const unitSize = 20
 const fieldWidthUnits = 25
 const fieldHeightUnits = 25
@@ -142,4 +297,4 @@ function animate(ms) {
 }
 
 window.requestAnimationFrame(animate)
-
+*/
