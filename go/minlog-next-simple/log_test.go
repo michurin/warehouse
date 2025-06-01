00a2c12f5f811db760cbeaeac094d86d087003cf
Alexey Michurin <a.michurin@gmail.com>
2025-06-01 20:43:50 +0300

    Minlog: one more idea: tests: example, asserts

--- go/minlog-next-simple/log_test.go
+++ go/minlog-next-simple/log_test.go
@@ -1,24 +1,36 @@
 package sslog_test
 
 import (
+	"context"
 	"errors"
 	"log/slog"
 	"os"
-	"testing"
 
 	"sslog"
 )
 
-func Test(t *testing.T) {
-	ctx := t.Context()
-	log := slog.New(slog.NewTextHandler(os.Stdout, nil))
+var handlerOptions = &slog.HandlerOptions{
+	AddSource: false,
+	ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey {
+			return slog.Attr{}
+		}
+		return a
+	},
+}
+
+func Example() {
+	ctx := context.Background()
+	log := slog.New(slog.NewTextHandler(os.Stdout, handlerOptions))
 	ctx = sslog.With(ctx, "K", "V")
 	ctx = sslog.With(ctx, slog.Group("g", "kk", "vv"))
 	log.Info("OK", sslog.Args(ctx)...)
 
 	err := errors.New("message")
-	err = sslog.Wrap(sslog.With(ctx, "E", "X"), err)
+	err = sslog.Wrap(err, sslog.Args(ctx))
 	log.Error("ERR", sslog.ArgsE(err)...)
 
-	// TODO ASSERTS
+	// output:
+	// level=INFO msg=OK K=V g.kk=vv
+	// level=ERROR msg=ERR K=V g.kk=vv
 }
