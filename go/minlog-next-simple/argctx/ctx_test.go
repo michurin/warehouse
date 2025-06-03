c1c51b82b114a54c52eb97711f54dac638763961
Alexey Michurin <a.michurin@gmail.com>
2025-06-03 03:15:50 +0300

    Minlog: one more idea: ctx: tests

---
+++ go/minlog-next-simple/argctx/ctx_test.go
@@ -0,0 +1,36 @@
+package argctx_test
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/michurin/minlog/argctx"
+)
+
+func Example() {
+	ctx := context.Background()
+	ctx = argctx.With(ctx, 1, "a", "b")
+	fmt.Println(argctx.Args(ctx))
+	// output:
+	// [1 a b]
+}
+
+func ExampleWith() {
+	ctx := context.Background()
+	ctx = argctx.With(ctx, 1)
+	ctx = argctx.With(ctx, "a", "b") // With can be called several times
+	ctx = argctx.With(ctx)           // no args are legal
+	fmt.Println(argctx.Args(ctx))
+	// output:
+	// [1 a b]
+}
+
+func ExampleArgs() {
+	ctx := context.Background()
+	fmt.Printf("%#v\n", argctx.Args(ctx)) // it is safe to call Args without With
+	ctx = argctx.With(ctx, 1, "a", "b")
+	fmt.Println(argctx.Args(ctx))
+	// output:
+	// []interface {}(nil)
+	// [1 a b]
+}
