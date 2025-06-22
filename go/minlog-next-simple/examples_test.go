6838af63410b6b28e2fadf0f5a082333c2dff66e
Alexey Michurin <a.michurin@gmail.com>
2025-06-22 09:54:09 +0300

    Minlog: one more idea: examples: zap

--- go/minlog-next-simple/examples_test.go
+++ go/minlog-next-simple/examples_test.go
@@ -6,6 +6,8 @@ import (
 	"log/slog"
 	"os"
 
+	"go.uber.org/zap"
+
 	"github.com/michurin/minlog/argctx"
 	"github.com/michurin/minlog/argerr"
 )
@@ -59,4 +61,53 @@ func Example_experimentalCtxGroups() {
 	// output:
 	// level=ERROR msg="error message" R=1 handler.H=2 handler.adapter.A=3
 	// level=INFO msg=OK R=1 handler.H=2 handler.adapter.A=3
+}
+
+func Example_zapGroups() {
+	ctx := context.Background()
+	log := zap.NewExample()
+
+	ctx = withInt(ctx, "R", 1)
+	ctx = argctx.With(ctx, func(x []any) any {
+		return zap.Dict("handler", zapCast(x)...)
+	})
+	ctx = withInt(ctx, "H", 2)
+	ctx = argctx.With(ctx, func(x []any) any {
+		return zap.Dict("adapter", zapCast(x)...)
+	})
+	ctx = withInt(ctx, "A", 3)
+
+	err := errors.New("error message")
+	err = argerr.Wrap(err, argctx.Args(ctx)...)
+
+	log.Error(err.Error(), zapFields(err)...) // we are obtaining all logging context from error only
+	log.Info("OK", zapFields(ctx)...)
+
+	// output:
+	// {"level":"error","msg":"error message","R":1,"handler":{"H":2,"adapter":{"A":3}}}
+	// {"level":"info","msg":"OK","R":1,"handler":{"H":2,"adapter":{"A":3}}}
+}
+
+func zapFields(x any) []zap.Field { // CONSIDER: split into two well typed functions?
+	switch t := x.(type) {
+	case error:
+		return zapCast(argerr.Args(t))
+	case context.Context:
+		return zapCast(argctx.Args(t))
+	}
+	return nil // CONSIDER: this case should not exist
+}
+
+func zapCast(a []any) []zap.Field {
+	f := []zap.Field(nil)
+	for _, x := range a {
+		if t, ok := x.(zap.Field); ok {
+			f = append(f, t)
+		}
+	}
+	return f
+}
+
+func withInt(ctx context.Context, key string, val int) context.Context { // CONSIDER: collection of wrappers? Generics?
+	return argctx.With(ctx, zap.Int(key, val))
 }
--- go/minlog-next-simple/go.mod
+++ go/minlog-next-simple/go.mod
@@ -1,3 +1,7 @@
 module github.com/michurin/minlog
 
 go 1.24.3
+
+require go.uber.org/zap v1.27.0
+
+require go.uber.org/multierr v1.10.0 // indirect
