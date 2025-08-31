2a024f8342bb82b00005e3ec18dd590c9f957b77
Alexey Michurin <a.michurin@gmail.com>
2025-08-31 15:55:25 +0300

    CV: building script: hints, how to set up LaTeX and all extras to build

--- cv/build.sh
+++ cv/build.sh
@@ -2,5 +2,11 @@
 
 # You may want to add to PATH something like this
 # /usr/local/texlive/2020/bin/x86_64-darwin
+#
+# Preparing Arch Linux:
+# pacman -Suy texlive-latex
+# pacman -Suy texlive-latexrecommended
+# pacman -Suy texlive-latexextra
+# pacman -Suy texlive-fontsextra
 
 pdflatex michurin.tex
