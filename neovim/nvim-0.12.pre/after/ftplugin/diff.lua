8209962eca9efb082f7e95d148a39724db0eccd1
Alexey Michurin <a.michurin@gmail.com>
2026-02-21 05:47:43 +0200

    Vimrc: neovim 0.12 (pre): ft=diff: folding: lua

--- neovim/nvim-0.12.pre/after/ftplugin/diff.lua
+++ neovim/nvim-0.12.pre/after/ftplugin/diff.lua
@@ -1,26 +1,17 @@
-vim.cmd([[
-setlocal foldmethod=expr foldexpr=DiffFold(v:lnum)
-function! DiffFold(lnum)
-  let line = getline(a:lnum)
-  if line =~ '^\(diff\|---\|+++\|@@\) '
-    return 1
-  elseif line[0] =~ '[-+ ]'
-    return 2
-  else
-    return 0
-  endif
-endfunction
-]])
+vim.opt_local.foldmethod = 'expr'
+vim.opt_local.foldexpr = 'v:lua.diff_fold()'
 
---[[
-vim.opt_local.foldmethod = "expr"
-vim.opt_local.foldexpr = "v:lua.DiffFold()"
-
-_G.DiffFold = function()
+function _G.diff_fold()
   local lnum = vim.v.lnum
   local line = vim.fn.getline(lnum)
-  if line:match("^(diff|%-%-%-|%+%+%+|@@) ") then return 1 end
-  if line:match("^[-+ ]") then return 2 end
+  if vim.startswith(line, 'diff ') then return 1 end
+  if vim.startswith(line, '--- ') then return 1 end
+  if vim.startswith(line, '+++ ') then return 1 end
+  if vim.startswith(line, '@@ ') then return 1 end
+  if vim.startswith(line, ' ') then return 2 end
+  if vim.startswith(line, '-') then return 2 end
+  if vim.startswith(line, '+') then return 2 end
   return 0
 end
-]]--
+
+vim.api.nvim_set_hl(0, 'Folded', { bg = '#262626', fg = '#ffffff' })
