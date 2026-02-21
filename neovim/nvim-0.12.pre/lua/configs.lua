202ad7fd6209e81a15547f98168d434ee74d9c03
Alexey Michurin <a.michurin@gmail.com>
2026-02-21 05:55:10 +0200

    Vimrc: neovim 0.12 (pre): just code formatting

--- neovim/nvim-0.12.pre/lua/configs.lua
+++ neovim/nvim-0.12.pre/lua/configs.lua
@@ -46,7 +46,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc     = 'Hightlight selection on yank',
   pattern  = '*',
   callback = function()
-    vim.highlight.on_yank {higroup = 'Visual', timeout = 500}
+    vim.highlight.on_yank { higroup = 'Visual', timeout = 500 }
   end,
 })
 
