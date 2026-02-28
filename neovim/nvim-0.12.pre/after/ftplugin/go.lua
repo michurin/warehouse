9bdc6f75f933d5b4a14aca1105e6dfa44d1c9c2b
Alexey Michurin <a.michurin@gmail.com>
2026-02-28 10:13:04 +0200

    Vimrc: neovim 0.12 (pre): go: cosmetcis

--- neovim/nvim-0.12.pre/after/ftplugin/go.lua
+++ neovim/nvim-0.12.pre/after/ftplugin/go.lua
@@ -2,9 +2,9 @@ vim.opt_local.expandtab = false
 
 vim.keymap.set('i', '.', '.<c-x><c-o>', { noremap = true, buffer = 0 })
 
-vim.opt_local.foldmethod = "expr"
-vim.opt_local.foldexpr = "v:lua.vim.lsp.foldexpr()"
-vim.opt_local.foldtext = "v:lua.vim.lsp.foldtext()"
+vim.opt_local.foldmethod = 'expr'
+vim.opt_local.foldexpr = 'v:lua.vim.lsp.foldexpr()'
+vim.opt_local.foldtext = 'v:lua.vim.lsp.foldtext()'
 vim.opt_local.foldlevel = 99
 vim.opt_local.foldlevelstart = 99
 vim.opt_local.foldenable = true
