0a8d3b7852a151ec9d22e5864253ec90343be47f
Alexey Michurin <a.michurin@gmail.com>
2026-03-01 20:49:33 +0200

    Vimrc: neovim 0.12 (pre): custom select: autofocus

--- neovim/nvim-0.12.pre/init.lua
+++ neovim/nvim-0.12.pre/init.lua
@@ -160,15 +160,22 @@ function _G.x(items, opts, callback)
   opts = opts or {}
   local prompt = opts.prompt or "Select"
   local format = opts.format_item or tostring
-  local max_height = opts.max_height or 12
+  local max_height = opts.max_height or 12 -- TODO hardcoded hard limit
   local title = ' ' .. prompt:gsub('^%s+', ''):gsub('[%s:]+$', '') .. ' '
 
+  -- print(vim.o.filetype) -- TODO: base highlighting and auto focusing on ft=go, title='Code actions'
+
   local labels = {}
-  for _, item in ipairs(items) do
-    table.insert(labels, format(item))
+  local jump_to = 1
+  for j, item in ipairs(items) do
+    local text = format(item)
+    if text:lower():match('fill') then -- TODO make this logic based on file type
+      jump_to = j
+    end
+    table.insert(labels, text)
   end
 
-  local buf = vim.api.nvim_create_buf(false, true)
+  local buf = vim.api.nvim_create_buf(false, true) -- listed=false, scratch=true (remove on win close)
   local width = #title
   for _, item in ipairs(labels) do
     width = math.max(width, vim.fn.strdisplaywidth(item))
@@ -192,18 +199,18 @@ function _G.x(items, opts, callback)
   vim.wo[win].number = false
   vim.wo[win].relativenumber = false
   vim.wo[win].signcolumn = 'no'
-  vim.bo[buf].bufhidden = 'wipe'
+  vim.bo[buf].bufhidden = 'wipe' -- important for auto removing
   vim.bo[buf].filetype = 'custom_select'
 
-  vim.fn.matchadd('Comment', [[^Fill]]) -- very experimental. What about scope of this definition?
-  vim.fn.matchadd('Statement', [[^Fill \zs\S\+\ze]])
+  vim.fn.matchadd('Comment', [[^Fill]])              -- win level. Use nvim_buf_add_highlight for buffer level
+  vim.fn.matchadd('Statement', [[^Fill \zs\S\+\ze]]) -- TODO make highlighting based on file type
   vim.fn.matchadd('Type', [[\zs"[^"]\+"\ze]])
 
   vim.bo[buf].modifiable = true
   vim.api.nvim_buf_set_lines(buf, 0, -1, false, labels)
   vim.bo[buf].modifiable = false
 
-  vim.api.nvim_win_set_cursor(win, { 1, 0 }) -- TODO?
+  vim.api.nvim_win_set_cursor(win, { jump_to, 0 })
 
   local function close()
     if vim.api.nvim_win_is_valid(win) then
