006dcbc4e08afed91872a273947fc5c2fca93c40
Alexey Michurin <a.michurin@gmail.com>
2026-03-15 12:53:22 +0200

    Vimrc: neovim 0.12 (pre): ft=lua: highlight "todo"

--- neovim/nvim-0.12.pre/after/ftplugin/lua.lua
+++ neovim/nvim-0.12.pre/after/ftplugin/lua.lua
@@ -9,3 +9,8 @@ vim.opt_local.foldtext = 'v:lua.vim.lsp.foldtext()'
 vim.opt_local.foldlevel = 99
 vim.opt_local.foldlevelstart = 99
 vim.opt_local.foldenable = true
+
+vim.cmd([[
+  syntax match myTodo /TODO/ containedin=luaComment
+  highlight link myTodo Todo
+]])
