5ef19212773fa35ce911fa39467cc99a3995df4a
Alexey Michurin <a.michurin@gmail.com>
2026-04-13 16:50:08 +0300

    Go: SSE: logging: client IP

--- go/sse-server-sent-events/loggingmw/mw.go
+++ go/sse-server-sent-events/loggingmw/mw.go
@@ -7,7 +7,7 @@ import (
 
 func MW(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s", r.Method, r.URL.String())
+		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.String())
 		next.ServeHTTP(w, r)
 	})
 }
