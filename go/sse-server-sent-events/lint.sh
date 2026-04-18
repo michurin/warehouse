9fa3bd830acb8b014f48df683bbcd56190a2c7e3
Alexey Michurin <a.michurin@gmail.com>
2026-04-18 04:04:56 +0300

    Go: SSE: static: eslint (legacy)

--- go/sse-server-sent-events/lint.sh
+++ go/sse-server-sent-events/lint.sh
@@ -1,3 +1,27 @@
-#!/bin/sh
+#!/bin/sh -xe
 
-./node_modules/.bin/eslint --no-config-lookup script.js
+# *** LEGACY APPROACH ***
+
+eslint \
+    --no-config-lookup \
+    --parser-options '{"ecmaVersion": 2018}' \
+    \
+    --global console \
+    --global window \
+    --global document \
+    --global location \
+    --global URLSearchParams \
+    \
+    --rule '{"arrow-body-style": "error"}' \
+    --rule '{"eqeqeq": ["error", "always"]}' \
+    --rule '{"indent": ["error", 2]}' \
+    --rule '{"no-undef": "error"}' \
+    --rule '{"no-unused-vars": "error"}' \
+    --rule '{"no-var": "error"}' \
+    --rule '{"one-var": ["error", "never"]}' \
+    --rule '{"prefer-arrow-callback": "error"}' \
+    --rule '{"prefer-const": "error"}' \
+    --rule '{"quotes": ["error", "single"]}' \
+    --rule '{"semi": ["error", "never"]}' \
+    "$@" \
+    $(find "$(dirname "$0")" -type f -name '*.js' | sort)
