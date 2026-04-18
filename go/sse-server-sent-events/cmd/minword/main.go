9ea594bacdd389c8f42f462d590e34706d700d38
Alexey Michurin <a.michurin@gmail.com>
2026-04-18 04:49:23 +0300

    Go: SSE: logging: init slog

--- go/sse-server-sent-events/cmd/minword/main.go
+++ go/sse-server-sent-events/cmd/minword/main.go
@@ -1,16 +1,33 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"log/slog"
 	"net/http"
+	"os"
+	"strings"
 
 	"sse/handler"
+	"sse/internal/xlog"
 	"sse/loggingmw"
 	"sse/room"
 )
 
 func main() {
-	// TODO mv it all to app.App(cfg)
+	slog.SetDefault(slog.New(xlog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{AddSource: true, ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+		if a.Key == slog.TimeKey {
+			a.Value = slog.StringValue(a.Value.Time().Format("2006-01-02_15:04:05"))
+		}
+		if a.Key == slog.SourceKey {
+			s := a.Value.Any().(*slog.Source)
+			i := strings.LastIndexByte(s.File, '/')
+			if i > 0 {
+				a.Value = slog.StringValue(fmt.Sprintf("%s:%d", s.File[i+1:], s.Line)) // s.Function?
+			}
+		}
+		return a
+	}}))))
 	house := room.New()
 	go handler.RevisionLoop(house)
 	err := http.ListenAndServe(":7011", loggingmw.MW(handler.Handler(house)))
