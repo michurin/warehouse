59d90680a2c98a80e95ee2cf22de3cd6bbd90897
Alexey Michurin <a.michurin@gmail.com>
2026-04-18 04:51:01 +0300

    Go: SSE: logging: mw

--- go/sse-server-sent-events/loggingmw/mw.go
+++ go/sse-server-sent-events/loggingmw/mw.go
@@ -1,13 +1,50 @@
 package loggingmw
 
 import (
-	"log"
+	"fmt"
+	"log/slog"
 	"net/http"
+	"time"
+
+	"sse/internal/xlog"
 )
+
+type wr struct {
+	status   int
+	location string
+	next     http.ResponseWriter
+}
+
+func (w *wr) Header() http.Header {
+	return w.next.Header()
+}
+
+func (w *wr) Write(b []byte) (int, error) {
+	return w.next.Write(b)
+}
+
+func (w *wr) WriteHeader(statusCode int) {
+	w.status = statusCode
+	w.location = w.next.Header().Get("Location")
+	w.next.WriteHeader(statusCode)
+}
 
 func MW(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("%s %s %s", r.RemoteAddr, r.Method, r.URL.String())
-		next.ServeHTTP(w, r)
+		ctx := r.Context()
+		ctx = xlog.WithRequestID(ctx)
+		ctx = xlog.WithAddr(ctx, r.RemoteAddr)
+		ctx = xlog.WithMethod(ctx, r.Method)
+		ctx = xlog.WithURL(ctx, r.URL.String())
+		wo := &wr{status: 200, next: w}
+		start := time.Now()
+		defer func() {
+			ctx = xlog.WithStatus(ctx, wo.status)
+			if len(wo.location) > 0 {
+				ctx = xlog.WithLocation(ctx, wo.location)
+			}
+			slog.InfoContext(ctx, fmt.Sprintf("%v", time.Since(start)))
+		}()
+		next.ServeHTTP(wo, r.WithContext(ctx))
 	})
 }
