25fb01585116af7ba04ec1184c8a4c6658fb1a36
Alexey Michurin <a.michurin@gmail.com>
2026-04-26 10:55:23 +0300

    Go: SSE: /stat: +pprof

--- go/sse-server-sent-events/internal/handlerstat/handlerstat.go
+++ go/sse-server-sent-events/internal/handlerstat/handlerstat.go
@@ -3,6 +3,7 @@ package handlerstat
 import (
 	"encoding/json"
 	"net/http"
+	"net/http/pprof"
 
 	"github.com/michurin/minchat/internal/xhouse"
 )
@@ -11,8 +12,22 @@ type Handler struct {
 	house *xhouse.House
 }
 
-func New(house *xhouse.House) *Handler {
-	return &Handler{house: house}
+func New(house *xhouse.House) *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /debug/pprof/", pprof.Index)
+	mux.HandleFunc("GET /debug/pprof/cmdline", pprof.Cmdline)
+	mux.HandleFunc("GET /debug/pprof/profile", pprof.Profile)
+	mux.HandleFunc("GET /debug/pprof/symbol", pprof.Symbol)
+	mux.HandleFunc("GET /debug/pprof/trace", pprof.Trace)
+	mux.Handle("GET /stat", &Handler{house: house})
+	mux.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/html; charset=utf-8")
+		w.Write([]byte(`<ul>
+<li><a href="/stat">/stat</a></li>
+<li><a href="/debug/pprof/">/debug/pprof/</a></li>
+</ul>`))
+	})
+	return mux
 }
 
 func (h *Handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
