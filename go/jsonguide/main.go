e7f0ba9c7afe4b8a4eee2226dacb06d28ae0797a
Alexey Michurin <a.michurin@gmail.com>
2026-05-01 10:09:03 +0300

    Go: jsonguide: cosmetics

--- go/jsonguide/main.go
+++ go/jsonguide/main.go
@@ -229,7 +229,7 @@ func isSimpleKey(s string) bool {
 	if len(s) == 0 {
 		return false
 	}
-	for i := 0; i < len(s); i++ {
+	for i := range len(s) {
 		c := s[i]
 		if !(c == '_' || c >= 'a' && c <= 'z' || c >= 'A' && c <= 'Z' || c >= '0' && c <= '9') {
 			return false
