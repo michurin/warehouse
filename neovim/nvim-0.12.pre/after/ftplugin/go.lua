2c1843b2c14d66f4e83f9e7720311ed3386f8cfb
Alexey Michurin <a.michurin@gmail.com>
2026-05-17 06:08:43 +0300

    Vimrc: neovim 0.12 (pre): go: macro (idea)

--- neovim/nvim-0.12.pre/after/ftplugin/go.lua
+++ neovim/nvim-0.12.pre/after/ftplugin/go.lua
@@ -8,3 +8,13 @@ vim.opt_local.foldtext = 'v:lua.vim.lsp.foldtext()'
 vim.opt_local.foldlevel = 99
 vim.opt_local.foldlevelstart = 99
 vim.opt_local.foldenable = true
+
+vim.keymap.set('n', '<space>cwv', function()
+  local line = 'ctx = context.WithValue(ctx, "", nil)'
+  local row, _ = unpack(vim.api.nvim_win_get_cursor(0))
+  vim.api.nvim_buf_set_lines(0, row, row, false, { line })
+  vim.lsp.buf.format({ async = false })
+  vim.api.nvim_win_set_cursor(0, { row + 1, 0 })
+  vim.cmd.normal({ 'f"f"', bang = true })
+  vim.cmd.startinsert()
+end)
