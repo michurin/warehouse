a017b18d2016bfad6673af338c6e19ff8336e882
Alexey Michurin <a.michurin@gmail.com>
2026-05-31 07:06:25 +0300

    Go: open telemetry exampels: simplest and liked traces

---
+++ go/opentelemetry-examples/vanila/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"context"
+	"time"
+
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/exporters/stdout/stdouttrace"
+	"go.opentelemetry.io/otel/sdk/resource"
+	sdktrace "go.opentelemetry.io/otel/sdk/trace"
+	"go.opentelemetry.io/otel/trace"
+)
+
+func initTracer() func(context.Context) error {
+	exporter, err := stdouttrace.New(stdouttrace.WithPrettyPrint()) // write to stdout
+	if err != nil {
+		panic(err)
+	}
+
+	tp := sdktrace.NewTracerProvider(
+		sdktrace.WithBatcher(exporter),
+		sdktrace.WithResource(resource.Default()),
+	)
+
+	otel.SetTracerProvider(tp)
+
+	return tp.Shutdown
+}
+
+func g(ctx context.Context) {
+	tracer := otel.Tracer("demo-tracer")
+
+	ctx, span := tracer.Start(ctx, "g")
+	defer span.End()
+
+	time.Sleep(1 * time.Second)
+}
+
+func f(ctx context.Context) {
+	tracer := otel.Tracer("demo-tracer")
+
+	ctx, span := tracer.Start(ctx, "f", trace.WithAttributes(attribute.String("CUSTOM", "VALUE")))
+	defer span.End()
+
+	g(ctx)
+	time.Sleep(1 * time.Second)
+}
+
+func main() {
+	ctx := context.Background()
+
+	shutdown := initTracer()
+	defer shutdown(ctx)
+
+	tracer := otel.Tracer("demo-tracer")
+
+	ctx, span := tracer.Start(ctx, "main")
+	defer span.End()
+
+	f(ctx)
+
+	time.Sleep(1 * time.Second)
+}
