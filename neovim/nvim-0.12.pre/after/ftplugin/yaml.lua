7227ac98877272494c1914effe9d330b2d157a3b
Alexey Michurin <a.michurin@gmail.com>
2026-08-08 06:22:40 +0300

    Vimrc: neovim 0.12 (pre): ft=yaml: spell checking

--- neovim/nvim-0.12.pre/after/ftplugin/yaml.lua
+++ neovim/nvim-0.12.pre/after/ftplugin/yaml.lua
@@ -3,3 +3,7 @@ vim.opt_local.shiftwidth = 2
 vim.opt_local.tabstop = 2
 vim.opt_local.softtabstop = 2
 vim.opt_local.foldmethod = 'indent'
+
+vim.opt_local.spell = true
+vim.opt_local.spellcapcheck = ""
+vim.opt_local.spelloptions = "camel"
